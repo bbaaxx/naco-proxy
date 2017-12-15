ab57eadd803935a675a2f914834d8c58b04aa70e
Ulises Torner <utorner@gmail.com>
2017-12-15 12:32:54 -0600

    Checkerboard background
    WIP

--- src/client/app/wood/wc-canvas/wc-canvas.js
+++ src/client/app/wood/wc-canvas/wc-canvas.js
@@ -21,10 +21,36 @@ export default class WcCanvas extends HTMLElement {
   }
 
   canvasLogic = (canvas: Object) => {
-    console.log('Holi pozoli ', canvas);
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    // get drawing context
     const ctx = canvas.getContext('2d');
-    ctx.fillRect(10, 10, 100, 100);
-    ctx.clearRect(15, 15, 100, 100);
-    ctx.strokeRect(20, 20, 100, 100);
+    // set canvas to window's dimentions
+    ctx.canvas.width = width;
+    ctx.canvas.height = height;
+    const res = 20;
+    const darkColor = '#eeeeee';
+    const lightColor = '#ffffff';
+    const length = width > height ? width / res : height / res;
+
+    for (let x = 0; x < res; x++) {
+      for (let y = 0; y < res; y++) {
+        let nx = x * length;
+        let ny = y * length;
+        if (x % 2 == 0 && y % 2 == 0) {
+          ctx.fillStyle = darkColor;
+        }
+        if (x % 2 == 0 && y % 2 == 1) {
+          ctx.fillStyle = lightColor;
+        }
+        if (x % 2 == 1 && y % 2 == 0) {
+          ctx.fillStyle = lightColor;
+        }
+        if (x % 2 == 1 && y % 2 == 1) {
+          ctx.fillStyle = darkColor;
+        }
+        ctx.fillRect(nx, ny, length, length);
+      }
+    }
   };
 }
