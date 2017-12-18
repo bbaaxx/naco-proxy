d14baac9deea29a41e166b1e343860fc79ef0eb8
Ulises Torner <utorner@gmail.com>
2017-12-18 09:52:57 -0600

    Merge issue solved

--- src/client/app/wood/wc-canvas/wc-canvas.js
+++ src/client/app/wood/wc-canvas/wc-canvas.js
@@ -28,29 +28,57 @@ export default class WcCanvas extends HTMLElement {
     // set canvas to window's dimentions
     ctx.canvas.width = width;
     ctx.canvas.height = height;
-    const res = 20;
+    var requestAnimationFrame =
+      window.requestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.msRequestAnimationFrame;
+
+    this.animate(ctx, requestAnimationFrame, width, height, 0, 0);
+  };
+
+  animate = (
+    context: any,
+    raf: any,
+    width: number,
+    height: number,
+    xShift: number,
+    yShift: number,
+  ) => {
+    //debugger;
+    //context.clearRect(0, 0, width, height);
+    console.log(context.constructor);
+
+    const res = 10; // bigger numbers yield smaller squares
     const darkColor = '#eeeeee';
     const lightColor = '#ffffff';
     const length = width > height ? width / res : height / res;
-
     for (let x = 0; x < res; x++) {
       for (let y = 0; y < res; y++) {
         let nx = x * length;
         let ny = y * length;
         if (x % 2 == 0 && y % 2 == 0) {
-          ctx.fillStyle = darkColor;
+          context.fillStyle = darkColor;
         }
         if (x % 2 == 0 && y % 2 == 1) {
-          ctx.fillStyle = lightColor;
+          context.fillStyle = lightColor;
         }
         if (x % 2 == 1 && y % 2 == 0) {
-          ctx.fillStyle = lightColor;
+          context.fillStyle = lightColor;
         }
         if (x % 2 == 1 && y % 2 == 1) {
-          ctx.fillStyle = darkColor;
+          context.fillStyle = darkColor;
         }
-        ctx.fillRect(nx, ny, length, length);
+        context.fillRect(nx + xShift, ny + yShift, length, length);
       }
     }
+
+    raf(
+      this.animate(context, raf, width, height, xShift + 0.01, yShift + 0.01),
+    );
+    /*setTimeout(
+      this.animate(context, raf, width, height, xShift + 0.001, yShift + 0.001),
+      30000,
+    );*/
   };
 }
